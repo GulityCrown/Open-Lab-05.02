d8158ece52b197707ef3e3856bfa0d1f5dfe1449
GulityCrown <109987666+GulityCrown@users.noreply.github.com>
2022-10-24 18:31:24 +0200

    Update StringTools.cs

--- Open-Lab-05.02/StringTools.cs
+++ Open-Lab-05.02/StringTools.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Open_Lab_05._02
 {
@@ -6,7 +6,29 @@ namespace Open_Lab_05._02
     {
         public string NoYelling(string sentence)
         {
-            throw new NotImplementedException();
+            for(int i = 0; i < sentence.Length; i++)
+            {
+
+                if (sentence[sentence.Length - 1] == sentence[sentence.Length - 2] && sentence[sentence.Length - 1] == '.')
+                {
+                    return sentence;
+
+                }
+                else if (sentence[sentence.Length - 1] != sentence[sentence.Length - 2])
+                {
+                    return sentence;
+
+                }
+                else
+                {
+                    while (sentence[sentence.Length - 1] == sentence[sentence.Length - 2])
+                    {
+                        sentence = sentence.Substring(0, sentence.Length - 1);
+                    }
+                }
+            
+            }
+            return sentence;
         }
     }
 }
